e4eda39de5cfdf041940c4f9a4a90112301d836c
chenchaofan <chen_chaofan@hotmail.com>
2019-08-20 23:01:33 +0800

    update

--- src/app/services/commonpopup.service.ts
+++ src/app/services/commonpopup.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
+import { ModalService, ToastService } from 'ng-zorro-antd-mobile';
 
 import {popupDataMap} from '../utilsmethod/popupData';
 
@@ -9,9 +10,22 @@ import {popupDataMap} from '../utilsmethod/popupData';
 export class CommonpopupService {
 
   constructor(private alertCtrl: AlertController,
-    private modalCtrl: ModalController) { }
+    private modalCtrl: ModalController,
+    private _zorromodal: ModalService, 
+    private _zorrotoast: ToastService) { }
 
   async popup(popupcode){
+    return new Promise((resolve, reject)=>{
+      ModalService.alert('请假类型', '', [
+        { text: '事假', onPress: () => this.zorroAlertHandlerOk(resolve, '事假') },
+        { text: '病假', onPress: () => this.zorroAlertHandlerOk(resolve, '病假') },
+        { text: '婚假', onPress: () => this.zorroAlertHandlerOk(resolve, '婚假') },
+        { text: '取消', onPress: () => this.zorroAlertHandlerCancel(resolve) }
+      ]);
+    })
+
+
+    return;
     //const alert = await this.alertCreate(popupcode);
     const alert = await this.alertCtrl.create({
       'mode': "ios",
@@ -55,5 +69,15 @@ export class CommonpopupService {
   // alert弹窗确定点击
   private alertHandlerOk(alertEl: HTMLIonAlertElement, d: any){
     alertEl.dismiss(d, 'ok').then();
+  }
+  // alert弹窗确定点击
+  private zorroAlertHandlerOk(resolve, d: any){
+    resolve.call(null, {
+      role: 'ok',
+      data: d
+    });
+  }
+  private zorroAlertHandlerCancel(resolve){
+    resolve.call(null, {role: 'cancel'});
   }
 }
