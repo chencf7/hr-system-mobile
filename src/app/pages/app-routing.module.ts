9327aaadeacc489f74929e0ca6b8de61463d585a
chenchaofan <chen_chaofan@hotmail.com>
2019-08-23 21:39:53 +0800

    update

--- src/app/pages/app-routing.module.ts
+++ src/app/pages/app-routing.module.ts
@@ -4,8 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'leave', loadChildren: './leave/leave.module#LeaveModule' },
   { path: 'test', loadChildren: './test/test.module#TestPageModule' },
+
 ];
 
 @NgModule({
