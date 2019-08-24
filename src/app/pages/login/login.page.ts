b90aec1b216794886f785908e40e77bd5ab570ec
chenchaofan <chen_chaofan@hotmail.com>
2019-08-24 23:04:53 +0800

    update

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  u_class: string='';
+  p_class: string='';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginunchange($event){
+    if($event.target.value){
+      this.u_class='has-value';
+    }else{
+      this.u_class='';
+    }
+  }
+  loginpwchange($event){
+    if($event.target.value){
+      this.p_class='has-value';
+    }else{
+      this.p_class='';
+    }
   }
 
 }
